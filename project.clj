474b7ed6144823efabb666758cb338b70a9e2e02
Oliver Godby <maleghast@gmail.com>
2019-04-08 16:19:14 +0100

    Forgot to update URL for License

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject earth.cervest/geotools "1.0.0"
+(defproject earth.cervest/geotools "1.0.1"
   :description "Clojure wrapper for existing OSS library for MGRS <--> lat/lon conversion"
   :url "http://github.com/Cervest/geotools"
   :license {:name "NASA OPEN SOURCE AGREEMENT VERSION 1.3"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url "https://opensource.org/licenses/NASA-1.3"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :java-source-paths ["javasrc"]
   :repl-options {:init-ns cervest.geotools.core})
